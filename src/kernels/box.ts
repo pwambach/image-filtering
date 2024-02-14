843b96052975fba84a9291d580f53b269f176adf
Philipp Wambach <philippwambach@googlemail.com>
2024-02-14 13:42:04 +0100

    update gh-pages

--- src/kernels/box.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Creates kernel values for a box/mean filter.
- * @param size The size (width) of the filter kernel
- * @returns A linear array of kernel values of a size x size grid
- */
-export function boxKernel(size: number): number[] {
-  const length = size ** 2;
-  return Array.from({ length }).fill(1 / length) as number[];
-}
